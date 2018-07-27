eaebbbec4f07fdeb9ea4913d008cfe1648df1e95
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-07-27 23:05:02 +0530

    k8s ha cluster working

--- k8s/bin/290.nginx.distribute.config.sh
+++ k8s/bin/290.nginx.distribute.config.sh
@@ -172,7 +172,6 @@ done
 
 source 101.setupNginx.sh
 
-
 kubectl create clusterrolebinding nginx --clusterrole cluster-admin --serviceaccount=nginx:default
 helm install --name nginx  --namespace nginx stable/nginx-ingress --set controller.service.type=NodePort --set controller.service.nodePorts.https=30443 --set controller.service.nodePorts.http=30080
 kubectl create namespace dev
