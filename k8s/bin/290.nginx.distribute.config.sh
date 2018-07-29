0bb6f967923521d95777e1e87bc64c044d907de5
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-07-29 11:25:40 +0530

    k8s ha cluster working

--- k8s/bin/290.nginx.distribute.config.sh
+++ k8s/bin/290.nginx.distribute.config.sh
@@ -49,8 +49,8 @@ kubectl create secret tls nginx-tls --namespace dev --key /etc/pki/nginx/private
 
 kubectl create clusterrolebinding nginx --clusterrole cluster-admin --serviceaccount=nginx:default
 
-#helm install --name nginx  --namespace nginx stable/nginx-ingress --set controller.service.type=NodePort --set controller.kind=daemonset --set controller.service.nodePorts.https=30443 --set controller.service.nodePorts.http=30080
-helm install --name nginx  --namespace nginx stable/nginx-ingress --set controller.service.type=NodePort --set controller.service.nodePorts.https=30443 --set controller.service.nodePorts.http=30080
+#helm upgrade --install nginx --namespace nginx stable/nginx-ingress --set controller.service.type=NodePort --set controller.kind=daemonset --set controller.service.nodePorts.https=30443 --set controller.service.nodePorts.http=30080
+helm upgrade --install nginx --namespace nginx stable/nginx-ingress --set controller.service.type=NodePort --set controller.service.nodePorts.https=30443 --set controller.service.nodePorts.http=30080
 
 
 kubectl --namespace dev run echoserver --image=gcr.io/google_containers/echoserver:1.4 --port=8080
