bee14304177c8e0a2cbed95a448a4dc3ef4c272e
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-19 11:34:18 +0530

    consul sample docker service

--- consul/docker-build.sh
+++ consul/docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t student-consul-service . student.Dockerfile
-docker build -t school-consul-service . school.Dockerfile
+docker build -t student-consul-service . -f student.Dockerfile
+docker build -t school-consul-service . -f school.Dockerfile
 
