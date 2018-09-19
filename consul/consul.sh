07d493b18b35bbd6762e1fca62f56537346048a7
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-19 14:21:36 +0530

    consul sample docker service

--- consul/consul.sh
+++ consul/consul.sh
@@ -5,3 +5,6 @@ docker run -d --rm --name=dev-consul -p 8446-8990:8446-8990 -e CONSUL_BIND_INTER
 docker run -d --rm --name=dev-consul-agent1 -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
 docker run -d --rm --name=dev-consul-agent2 -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
 
+
+
+dig @172.17.0.2 -p 8600 school-service.service.dc1.consul. ANY
