3c73ffb4c875718418e183a3df4dd68d1b2bb4b5
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-19 13:00:07 +0530

    consul sample docker service

--- consul/consul.sh
+++ consul/consul.sh
@@ -1,9 +1,7 @@
-
-
-IP: eth0: 172.17.0.2
-
+#!/bin/sh
 docker rm -f dev-consul dev-consul-agent1 dev-consul-agent2
 
 docker run -d --rm --name=dev-consul -p 8446-8990:8446-8990 -e CONSUL_BIND_INTERFACE=eth0 consul
 docker run -d --rm --name=dev-consul-agent1 -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
 docker run -d --rm --name=dev-consul-agent2 -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
+
