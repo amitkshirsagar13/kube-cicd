0bd193720a59e1262436b7f85af76531bcc54e1e
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-19 14:06:40 +0530

    consul sample docker service

--- consul/docker-consul-sample.sh
+++ consul/docker-consul-sample.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 docker rm -f school-service student-service 
+docker network disconnect --force bridge school-service student-service 
+
 docker run -d --rm --name student-service -p 8999:8999 -e VPORT=8999 student-consul-service
 docker run -d --rm --name school-service -p 8998:8998 -e VPORT=8998 school-consul-service
