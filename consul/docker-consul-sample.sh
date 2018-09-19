cf7c0c1e434964959284da08fc436aa4c903c92e
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-09-19 11:39:07 +0530

    consul sample docker service

--- consul/docker-consul-sample.sh
+++ consul/docker-consul-sample.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run -d --rm -name student-service -p 8899:8899 tudent-consul-service
-docker run -d --rm -name school-service -p 8898:8898 school-consul-service
+docker run -d --rm --name student-service -p 8899:8899 tudent-consul-service
+docker run -d --rm --name school-service -p 8898:8898 school-consul-service
