cf9e43a5053386a12cb2e4639e0272fe67b0fad9
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-10-10 21:12:24 +0530

    jenkins rebuilt

--- jenkins/buildJenkinsImages.sh
+++ jenkins/buildJenkinsImages.sh
@@ -5,3 +5,9 @@ docker build -t amitkshirsagar13/k8s-jenkins-slave . -f k8s-jenkins-slave/Docker
 docker build -t amitkshirsagar13/k8s-jenkins-dotnet . -f k8s-jenkins-dotnet/Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-alpine . -f k8s-jenkins-alpine/Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-job-builder . -f k8s-jenkins-job-builder/Dockerfile
+docker push amitkshirsagar13/k8s-jenkins-plugin
+docker push amitkshirsagar13/k8s-jenkins-master
+docker push amitkshirsagar13/k8s-jenkins-slave
+docker push amitkshirsagar13/k8s-jenkins-dotnet
+docker push amitkshirsagar13/k8s-jenkins-alpine
+docker push amitkshirsagar13/k8s-jenkins-job-builder
