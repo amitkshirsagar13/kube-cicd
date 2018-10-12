9c7dd8843c1a0264642374006844715c6c6239cc
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2018-10-12 20:09:19 +0530

    jenkins rebuilt

--- jenkins/buildJenkinsImages.sh
+++ jenkins/buildJenkinsImages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t amitkshirsagar13/k8s-jenkins-plugin . -f k8s-plugins.Dockerfile
+docker build -t amitkshirsagar13/k8s-jenkins-master-init . -f k8s-jenkins-master-init/Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-master . -f k8s-jenkins.Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-slave . -f k8s-jenkins-slave/Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-dotnet . -f k8s-jenkins-dotnet/Dockerfile
@@ -7,9 +8,8 @@ docker build -t amitkshirsagar13/k8s-jenkins-alpine . -f k8s-jenkins-alpine/Dock
 docker build -t amitkshirsagar13/k8s-jenkins-job-builder . -f k8s-jenkins-job-builder/Dockerfile
 docker build -t amitkshirsagar13/k8s-jenkins-kubernetes . -f k8s-jenkins-kubernetes/Dockerfile
 
-docker build -t amitkshirsagar13/k8s-jenkins-master-init . -f k8s-jenkins-master-init/Dockerfile
-
 docker push amitkshirsagar13/k8s-jenkins-plugin
+docker push amitkshirsagar13/k8s-jenkins-master-init
 docker push amitkshirsagar13/k8s-jenkins-master
 docker push amitkshirsagar13/k8s-jenkins-slave
 docker push amitkshirsagar13/k8s-jenkins-dotnet
