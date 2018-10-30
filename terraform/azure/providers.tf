ae900eba2a3593558a6d4b347f22feab5c357319
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-10-30 19:50:00 +0530

    Update providers.tf

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  subscription_id = "90d038f7-0e82-4143-94e9-0e0e767dd077"
-  client_id       = "81f9af9f-1d36-4d7e-a368-dcfc96b77813"
-  client_secret   = "854uWSV1CBEPICuAzNIkpsdl+tmtettWqkjHhpkWBm0="
-  tenant_id       = "90d038f7-0e82-4143-94e9-0e0e767dd077"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
